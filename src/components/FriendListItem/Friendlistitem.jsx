dd623fbf8b87827a4adebccda3dcb8d26b36e53a
KonstantinYev <109310447+KonstantinYevchuk@users.noreply.github.com>
2023-02-14 09:39:59 +0200

    add css frofileList css

--- src/components/FriendListItem/Friendlistitem.jsx
+++ src/components/FriendListItem/Friendlistitem.jsx
@@ -2,9 +2,10 @@ import PropTypes from 'prop-types';
 import css from 'components/FriendListItem/Friendlistitem.module.css';
 
 export const FriendListItem = ({avatar, name, isOnline}) => {
+    console.log(css[isOnline]);
     return (
         <li className={css.item}>
-        <span className="status">{isOnline}</span>
+        <span className={`${css.status} ${css[isOnline]}`}>{isOnline}</span>
         <img className={css.avatar} src={avatar} alt={name} width="48" />
         <p className="name">{name}</p>
         </li>
